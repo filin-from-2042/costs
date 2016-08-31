170b607536208b6c2ec77b56d304f03464fcb9a6
filin-from-2042 <filin2042@yandex.ru>
2016-08-31 22:03:38 +0300

    переделка на cameraCaptureTask

--- costs/Database.cs
+++ costs/Database.cs
@@ -153,7 +153,7 @@ namespace costs
         //// Define completion value: private field, public property and database column.
         private byte[] _photo;
 
-        [Column]
+        [Column(DbType = "image")]
         public byte[] Photo
         {
             get
