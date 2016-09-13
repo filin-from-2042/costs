6f721750e55b0fc06b3731c138382318c99a352a
konov <konov@konor.ru>
2016-09-13 12:22:05 +0300

    Добавлено сохранение конечной даты просматриваемого диапазона (забыл в первый раз это сделать)

--- costs/MainPage.xaml.cs
+++ costs/MainPage.xaml.cs
@@ -28,15 +28,85 @@ namespace costs
             this.DataContext = this;
         }
 
+        //--------------------------------------------------- STANDART PAGE EVENTS -------------------------------------------------------
         protected override void OnNavigatedTo(System.Windows.Navigation.NavigationEventArgs e)
         {
             DateTextBlock.Text = DateTime.Now.ToString("D", new CultureInfo("ru-RU"));
             if (PhoneApplicationService.Current.State.ContainsKey("startRangeDP"))
-                startRangeDP.Value=Convert.ToDateTime(PhoneApplicationService.Current.State["startRangeDP"].ToString());
+                startRangeDP.Value = Convert.ToDateTime(PhoneApplicationService.Current.State["startRangeDP"].ToString());
+            if (PhoneApplicationService.Current.State.ContainsKey("endRangeDP"))
+                endRangeDP.Value = Convert.ToDateTime(PhoneApplicationService.Current.State["endRangeDP"].ToString());
+
             else startRangeDP.Value = new DateTime(DateTime.Now.Year, DateTime.Now.Month, 1);
             fillPieChart();
             fillConsumptionsList(startRangeDP.Value, endRangeDP.Value);            
             base.OnNavigatedTo(e);
+        }
+
+        //--------------------------------------------------- CONTROLS EVENTS ------------------------------------------------------------
+
+        private void startRangeDP_ValueChanged(object sender, DateTimeValueChangedEventArgs e)
+        {
+            PhoneApplicationService.Current.State["startRangeDP"] = startRangeDP.Value.Value.ToShortDateString();
+        }
+
+        private void endRangeDP_ValueChanged(object sender, DateTimeValueChangedEventArgs e)
+        {
+            PhoneApplicationService.Current.State["endRangeDP"] = endRangeDP.Value.Value.ToShortDateString();
+        }
+
+        private void ApplicationBarIconButton_Click_1(object sender, EventArgs e)
+        {
+            NavigationService.Navigate(new Uri("/AddLoss.xaml", UriKind.RelativeOrAbsolute));
+        }
+
+        #region INotifyPropertyChanged Members
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void NotifyPropertyChanged(string propertyName)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+        #endregion
+
+        private void consumptionsListBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {            
+            ListBox lb = sender as ListBox;
+            int categoryId = Convert.ToInt32(lb.SelectedItem.GetType().GetProperty("CategoryID").GetValue(lb.SelectedItem, null));
+            string categoryName = Convert.ToString(lb.SelectedItem.GetType().GetProperty("ConsumptionCategory").GetValue(lb.SelectedItem, null));
+            float count = Convert.ToSingle(lb.SelectedItem.GetType().GetProperty("SummCount").GetValue(lb.SelectedItem, null));
+            string startDate = startRangeDP.Value.Value.Date.ToShortDateString();
+            string endDate = endRangeDP.Value.Value.Date.ToShortDateString();
+
+            NavigationService.Navigate(new Uri("/DetailCost.xaml?categoryId=" + categoryId.ToString() + "&categoryName=" + categoryName + "&count=" + count.ToString() + "&startDate=" + startDate + "&endtDate=" + endDate, UriKind.RelativeOrAbsolute));
+        }
+
+        //--------------------------------------------------------------- GENERAL FUNCTIONS --------------------------------------------------
+
+
+        protected void fillConsumptionsList(DateTime? startDate, DateTime? endDate)
+        {
+            // TODO: сделать нормальный select new, при получении из ListBox получается неопнятный объект
+            if (startDate == null || endDate == null) return;
+
+            var consumptionsInDB = (from Consumption consumptions in costsDB.Consumptions
+                                    join Category categories in costsDB.Categories on consumptions.CategoryId equals categories.CategoryId
+                                    where consumptions.CreateDate.Date >= startDate.Value.Date && consumptions.CreateDate.Date <= endDate.Value.Date && consumptions.Count > 0
+                                    group consumptions by new { categories.CategoryName, categories.CategoryId } into consumptionGroupped
+                                    //select consumptionGroupped;
+                                    select new { CategoryID = consumptionGroupped.Key.CategoryId, ConsumptionCategory = consumptionGroupped.Key.CategoryName, SummCount = consumptionGroupped.Sum(i => i.Count) })
+                                   .Union(
+                                    from Consumption consumptions in costsDB.Consumptions
+                                    join Category categories in costsDB.Categories on consumptions.CategoryId equals categories.CategoryId
+                                    where consumptions.CreateDate.Date >= startDate.Value.Date && consumptions.CreateDate.Date <= endDate.Value.Date && consumptions.Count < 0
+                                    group consumptions by new { categories.CategoryName, categories.CategoryId } into consumptionGroupped
+                                    //select consumptionGroupped;
+                                    select new { CategoryID = consumptionGroupped.Key.CategoryId, ConsumptionCategory = consumptionGroupped.Key.CategoryName, SummCount = consumptionGroupped.Sum(i => i.Count) }
+                                    );
+            consumptionsListBox.ItemsSource = consumptionsInDB;
         }
 
         private void fillPieChart()
@@ -68,69 +138,6 @@ namespace costs
                 PieChart.DataSource = Data;
             }
             else PieChart.Visibility = System.Windows.Visibility.Collapsed;
-        }
-
-        private void ApplicationBarIconButton_Click_1(object sender, EventArgs e)
-        {
-            NavigationService.Navigate(new Uri("/AddLoss.xaml", UriKind.RelativeOrAbsolute));
-        }
-
-        protected void fillConsumptionsList(DateTime? startDate, DateTime? endDate)
-        {
-            // TODO: сделать нормальный select new, при получении из ListBox получается неопнятный объект
-            if (startDate == null || endDate == null) return;
-            
-            var consumptionsInDB = (from Consumption consumptions in costsDB.Consumptions
-                                    join Category categories in costsDB.Categories on consumptions.CategoryId equals categories.CategoryId
-                                    where consumptions.CreateDate.Date >= startDate.Value.Date && consumptions.CreateDate.Date <= endDate.Value.Date && consumptions.Count > 0
-                                    group consumptions by new { categories.CategoryName, categories.CategoryId } into consumptionGroupped
-                                    //select consumptionGroupped;
-                                    select new { CategoryID = consumptionGroupped.Key.CategoryId, ConsumptionCategory = consumptionGroupped.Key.CategoryName, SummCount = consumptionGroupped.Sum(i => i.Count) })
-                                   .Union(
-                                    from Consumption consumptions in costsDB.Consumptions
-                                    join Category categories in costsDB.Categories on consumptions.CategoryId equals categories.CategoryId
-                                    where consumptions.CreateDate.Date >= startDate.Value.Date && consumptions.CreateDate.Date <= endDate.Value.Date && consumptions.Count < 0
-                                    group consumptions by new { categories.CategoryName, categories.CategoryId } into consumptionGroupped
-                                    //select consumptionGroupped;
-                                    select new { CategoryID = consumptionGroupped.Key.CategoryId, ConsumptionCategory = consumptionGroupped.Key.CategoryName, SummCount = consumptionGroupped.Sum(i => i.Count) }
-                                    );
-                      
-            //var consumptionsInDB = from Consumption consumptions in costsDB.Consumptions
-            //                        join Category categories in costsDB.Categories on consumptions.CategoryId equals categories.CategoryId
-            //                        where consumptions.CreateDate.Date >= startDate.Value.Date && consumptions.CreateDate.Date <= endDate.Value.Date && consumptions.Count > 0
-            //                        group consumptions by new { categories.CategoryName, categories.CategoryId } into consumptionGroupped
-            //                        //select consumptionGroupped;
-            //                        select new { CategoryID = consumptionGroupped.Key.CategoryId, ConsumptionCategory = consumptionGroupped.Key.CategoryName, SummCount = consumptionGroupped.Sum(i => i.Count) };
-            consumptionsListBox.ItemsSource = consumptionsInDB;
-        }
-
-        #region INotifyPropertyChanged Members
-
-        public event PropertyChangedEventHandler PropertyChanged;
-        private void NotifyPropertyChanged(string propertyName)
-        {
-            if (PropertyChanged != null)
-            {
-                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
-            }
-        }
-        #endregion
-
-        private void consumptionsListBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
-        {            
-            ListBox lb = sender as ListBox;
-            int categoryId = Convert.ToInt32(lb.SelectedItem.GetType().GetProperty("CategoryID").GetValue(lb.SelectedItem, null));
-            string categoryName = Convert.ToString(lb.SelectedItem.GetType().GetProperty("ConsumptionCategory").GetValue(lb.SelectedItem, null));
-            float count = Convert.ToSingle(lb.SelectedItem.GetType().GetProperty("SummCount").GetValue(lb.SelectedItem, null));
-            string startDate = startRangeDP.Value.Value.Date.ToShortDateString();
-            string endDate = endRangeDP.Value.Value.Date.ToShortDateString();
-
-            NavigationService.Navigate(new Uri("/DetailCost.xaml?categoryId=" + categoryId.ToString() + "&categoryName=" + categoryName + "&count=" + count.ToString() + "&startDate=" + startDate + "&endtDate=" + endDate, UriKind.RelativeOrAbsolute));
-        }
-
-        private void startRangeDP_ValueChanged(object sender, DateTimeValueChangedEventArgs e)
-        {
-            PhoneApplicationService.Current.State["startRangeDP"] = startRangeDP.Value.Value.ToShortDateString();
         }
 
     }
